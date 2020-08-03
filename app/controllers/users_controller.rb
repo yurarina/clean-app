b3d9df8011ca06c3cf3e805dc5af643975335cdd
yurarina <yurarina000@gmail.com>
2020-08-03 15:03:43 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  def detail
+    @user = User.find_by(id: params[:id])
+  end
+
+  def after_sign_up_path_for(resource)
+    "/user/#{current_user.id}"
+  end
 end
